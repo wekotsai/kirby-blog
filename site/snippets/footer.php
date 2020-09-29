e0e6f28fb00e98e79cd115aafa5f7c78986b4348
Wéko <42733086+wekotsai@users.noreply.github.com>
2020-09-29 20:19:10 +0200

    add icons

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,14 @@
 <footer class="footer">
-    <?php echo $site->footercopyright() ?>
+  <div class="socialmedia">  
+    <?php foreach ($site->socialmedia()->toStructure() as $link) : ?>
+      <a href="<?php echo $link->url() ?>" aria-label="<?php echo $link->title() ?>" title="<?php echo $link->title() ?>" 
+      <?php echo preg_match('/^http/', $link->url()) ? 'target="_blank" rel="noopener"' : '' ?>
+        >
+        <img src="<?php echo $link->icon()->toFile()->url() ?>">
+      </a>
+    <?php endforeach ?>
+  </div>
+  <p class="copyright"><?php echo $site->footercopyright() ?></p>
 </footer>
 
 </body>
