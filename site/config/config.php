2a970c81c339fb3d21922e2ed5acff27357a3418
Wéko <42733086+wekotsai@users.noreply.github.com>
2021-01-18 17:32:23 +0100

    📝 change output folder

--- site/config/config.php
+++ site/config/config.php
@@ -5,7 +5,7 @@ return [
     'd4l' => [
       'static_site_generator' => [
         'endpoint' => 'generate_static_site', # set to any string like 'generate-static-site' to use the built-in endpoint (necessary when using the blueprint field)
-        'output_folder' => './static', # you can specify an absolute or relative path
+        'output_folder' => '../kirby-blog-static', # you can specify an absolute or relative path
         'preserve' => [], # preserve individual files / folders in the root level of the output folder (anything starting with "." is always preserved)
         'base_url' => '/', # if the static site is not mounted to the root folder of your domain, change accordingly here
         'skip_media' => false, # set to true to skip copying media files, e.g. when they are already on a CDN; combinable with 'preserve' => ['media']
